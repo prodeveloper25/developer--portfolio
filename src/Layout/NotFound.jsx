9fcda44063f9919326896f48f7d617f36cff402a
ProDeveloperBD2 <mdnahid.prg@gmail.com>
2024-01-06 10:44:11 +0600

    not found setup responsivnes finished

--- src/Layout/NotFound.jsx
+++ src/Layout/NotFound.jsx
@@ -5,20 +5,23 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="w-full px-4 mt-28 ">
-      <div className="flex justify-center h-[300px] items-center my-auto">
-        <div className="lg:w-[560px]  h-[300px] w-full">
-          <Lottie className="h-full" animationData={notFoundImg}></Lottie>
+    <div className="w-full h-full">
+      <div className="w-full min-h-screen flex justify-center  items-center">
+        <div className="lg:w-[560px]  h-[330px] w-full">
+          <Lottie
+            className="h-full w-full"
+            animationData={notFoundImg}
+          ></Lottie>
+          <div className="flex justify-center items-center">
+            <Link
+              to="/"
+              className="bg-[#fc3a40] cursor-pointer  flex justify-center gap-2 items-center text-white px-8 lg:py-3  md:py-3 py-2 lg:text-lg lg:font-semibold duration-300 rounded hover:bg-[#14a800] "
+            >
+              <FaHome className="lg:text-xl text-lg"></FaHome>{" "}
+              <span>Back to Home</span>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center items-center">
-        <Link
-          to="/"
-          className="bg-[#14a800] cursor-pointer  flex justify-center gap-2 items-center text-white px-8 lg:py-3  md:py-3 py-2 lg:text-lg lg:font-semibold duration-300 rounded hover:bg-[#fc3a40] "
-        >
-          <FaHome className="lg:text-xl text-lg"></FaHome>{" "}
-          <span>Back to Home</span>
-        </Link>
       </div>
     </div>
   );
