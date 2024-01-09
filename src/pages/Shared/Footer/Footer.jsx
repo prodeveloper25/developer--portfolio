689803c3698564caaf8843bac4dcc160bc233138
ProDeveloperBD2 <mdnahid.prg@gmail.com>
2024-01-09 19:02:56 +0600

    responsive plane I mean back to top button

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-import logoImg from "../../../assets/icon/logo2.png";
 import {
   FaBook,
   FaFacebook,
@@ -14,14 +12,16 @@ import {
   FaTwitter,
 } from "react-icons/fa";
 import { FaMessage } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 import ScrollToTop from "react-scroll-to-top";
+import logoImg from "../../../assets/icon/logo2.png";
 
 const Footer = () => {
   return (
     <>
       <ScrollToTop
         smooth
-        top="300"
+        top="310"
         svgPath="M128,216,88,232V200l16-16V152L24,168V136l80-40V48a24,24,0,0,1,48,0V96l80,40v32l-80-16v32L168,200v32Z"
         className="pl-[6px] bg-red-100 duration-1000"
         color="#14a800"
